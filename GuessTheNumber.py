0685e75b9cba345e100ed7aa6893658fbfb6d776
ayush41 <anonymousking41681@gmail.com>
2024-09-18 02:08:16 +0530

    cpu to guess

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -12,4 +12,26 @@ def guess(x):
             print('Too high')
     print(f'Congo. you got guessed the correct num {random_number} correctly')
 
-guess(20)
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low,high)
+        else:
+            guess = low
+        
+        feedback = input(f'Is {guess} too high (h), too low (l), or correct (C)').lower()
+
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+        
+    print(f'Computer guessed it corerctly the number, {guess}')
+
+
+computer_guess(10)
+
+
