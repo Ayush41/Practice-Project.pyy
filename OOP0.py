22a06f2eb84f4eaecedc320a6a36eed8c178895f
ayush41 <anonymousking41681@gmail.com>
2024-11-14 20:00:07 +0530

    inheritence

--- OOP0.py
+++ OOP0.py
@@ -1,1 +1,26 @@
 # Inheritence in pyhton
+
+class Person(object):
+    # Constructor
+    def __init__(self,name):
+        self.name = name
+    # Get Name 
+    def getName(self):
+        return self.name
+    
+    # To check if this person is an emp
+    def isEmp(self):
+        return False
+
+# Inherited from person class
+class Employee(Person):
+    def isEmp(self):
+        return True
+    
+# Main Code
+
+emp = Person("Itachi")
+print(emp.getName(),emp.isEmp())
+
+emp = Employee("Naruto")
+print(emp.getName(),emp.isEmp())
