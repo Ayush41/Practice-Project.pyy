e513c11ee19b0fc0bccfec42a4e554fd57780744
ayush41 <anonymousking41681@gmail.com>
2024-11-16 00:46:18 +0530

    jokes added

--- JokeGenerator.py
+++ JokeGenerator.py
@@ -4,8 +4,16 @@ import random
 
 def Joke():
     jokes = [
-        
+        "Why don't skeletons fight each other? They don't have the guts.",
+        "I told my wife she was drawing her eyebrows too high. She looked surprised.",
+        "I used to play piano by ear, but now I use my hands!",
+        "Why don’t programmers like nature? It has too many bugs."
+        "What do kids play when their mom is using the phone? Bored games.",
+        "What do you call an ant who fights crime? A vigilANTe!",
+        "Why are snails slow? Because they’re carrying a house on their back.",
+        "What’s the smartest insect? A spelling bee!"
     ]
+    
     print(random.choice(jokes))
 
 Joke()
